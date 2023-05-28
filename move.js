cf546544221826fac955795fa90e815426ed4f96
kengo <test@example.com>
2023-05-28 11:44:27 +0900

    初期ファイルデータの投入

---
+++ move.js
@@ -0,0 +1,43 @@
+$(".slider").slick({
+  autoplay: true, //自動的に動き出すか。初期値はfalse。
+  infinite: true, //スライドをループさせるかどうか。初期値はtrue。
+  dots: true, //下部ドットナビゲーションの表示
+  arrows: false,
+  centerMode: true,
+});
+$(".openbtn").click(function () {
+  //ボタンがクリックされたら
+  $(this).toggleClass("active"); //ボタン自身に activeクラスを付与し
+  $("#g-nav").toggleClass("panelactive"); //ナビゲーションにpanelactiveクラスを付与
+});
+
+$("#g-nav a").click(function () {
+  //ナビゲーションのリンクがクリックされたら
+  $(".openbtn").removeClass("active"); //ボタンの activeクラスを除去し
+  $("#g-nav").removeClass("panelactive"); //ナビゲーションのpanelactiveクラスも除去
+});
+
+// 動きのきっかけとなるアニメーションの名前を定義
+function fadeAnime() {
+  //ふわっと動くきっかけのクラス名と動きのクラス名の設定
+  $(".fadeUpTrigger").each(function () {
+    //fadeInUpTriggerというクラス名が
+    var elemPos = $(this).offset().top - 50; //要素より、50px上の
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight) {
+      $(this).addClass("fadeUp"); // 画面内に入ったらfadeInというクラス名を追記
+    }
+    });
+
+  } //ここまでふわっと動くきっかけのクラス名と動きのクラス名の設定
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+  fadeAnime(); /* アニメーション用の関数を呼ぶ*/
+}); // ここまで画面をスクロールをしたら動かしたい場合の記述
+
+// 画面が読み込まれたらすぐに動かしたい場合の記述
+$(window).on("load", function () {
+  fadeAnime(); /* アニメーション用の関数を呼ぶ*/
+}); // ここまで画面が読み込まれたらすぐに動かしたい場合の記述
